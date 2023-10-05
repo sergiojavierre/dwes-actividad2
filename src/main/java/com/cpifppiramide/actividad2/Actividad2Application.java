63508a2a9fcde6bce97ef2401b83c59672a29e44
sergiojavierre <sergio.javierre@fppiramide.com>
2023-10-05 10:08:32 +0200

    Versión con MySQL

--- src/main/java/com/cpifppiramide/actividad2/Actividad2Application.java
+++ src/main/java/com/cpifppiramide/actividad2/Actividad2Application.java
@@ -1,6 +1,5 @@
 package com.cpifppiramide.actividad2;
 
-import com.cpifppiramide.actividad2.domain.Coche;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Actividad2Application {
 
-	//version 1.0
+	//version 1.1
 	public static void main(String[] args) {
 		SpringApplication.run(Actividad2Application.class, args);
 	}
